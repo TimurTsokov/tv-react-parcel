0394fc5b25fc2a1cd4c1b7e4b15c908cb0029f43
TimurTsokov <tsokov2t1m5@gmail.com>
2018-12-01 22:25:30 +0200

    commit tv-branch work

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import {BrowserRouter as Router} from 'react-router-dom';
 import AuthContainer from './Containers/AuthContainer/AuthContainer';
+import { square } from './lib';
 
 class App extends Component {
     render() {
